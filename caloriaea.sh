57157307f4f1fe44700910669f1477afff23c1a8
paulohml <paulo3385@hotmail.com>
2020-09-22 23:00:49 -0400

    funcionalidades implementadas - a partir de agora fazer melhorias

--- caloriaea.sh
+++ caloriaea.sh
@@ -15,7 +15,7 @@
 
 #Versão do script
 # ===============
-VERSION="0.1"
+VERSION="0.2"
 
 #Flag de loop (s = Sim; n = Não)
 # ==============================
@@ -57,8 +57,8 @@ do
 			# Acusará erro caso a lista de contatos não exista
 			# ================================================
 				"Listar")
-				if [[ -e "contatos.txt" ]]; then
-					dialog --title 'Entradas' --textbox contatos.txt 0 0
+				if [[ -e "cofredecalorias.txt" ]]; then
+					dialog --title 'Cofre' --textbox cofredecalorias.txt 0 0
 				else
 					dialog --title 'Erro!' --msgbox 'Lista de entradas não existe!' 6 45
 				fi
@@ -67,26 +67,65 @@ do
 			# Acusará erro quando a entrada já existir ou usuário não inserir valor
 			# =====================================================================
 				"Adicionar")
-					NAME=$( dialog \
+					IDENTIFICACAO=$( dialog \
 						--stdout \
-						--title 'Qdd de calorias - Quilometragem - Duração' \
-						--inputbox 'Adicione a Qdd de calorias - Quilometragem - Duração' \
+						--title 'Identificação' \
+						--inputbox 'Digite a sua Identificação!' \
 						0 0)
 
-					if [[ -z "$NAME" ]]; then
-						dialog --title 'Erro!' --msgbox 'Nenhum nome inserido!' 6 45
+					if [[ -z "$IDENTIFICACAO" ]]; then
+						dialog --title 'Erro!' --msgbox 'Nenhum identificação inserida!' 6 45
 					else
-						ENTRADA=$(cat contatos.txt |
-							grep "$NAME:" |
+						ENTRADA=$(cat cofredecalorias.txt |
+							grep "$IDENTIFICACAO:" |
 							cut -d ":" -f1)
 
-						if [[ -z "$ENTRADA" ]]; then
-							DATA=$(dialog \
-							)
+						if [[ -z "$CALORIAS" ]]; then
+							CALORIAS=$(dialog \
+								--stdout \
+								--title 'Calorias' \
+								--inputbox 'Adicione o número de calorias perdidas em seu treino!' \
+							0 0)
+
+							if [[ -z "$CALORIAS" ]]; then
+								dialog --title 'Erro!' --msgbox 'Nenhuma contagem de calorias feita!' 6 45
+							else
+								echo "$IDENTIFICACAO: $CALORIAS" >> cofredecalorias.txt
+								dialog --title 'Adicionado!' --msgbox 'Contagem feita com sucesso!' 6 45
+							fi
+						else
+							dialog --title 'Erro!' --msgbox 'Contato já existe!' 6 45
+						fi
+					fi
+					;;
+
+				# Opção de Remover 
+				# Acusará erro caso contato não exista ou usuário não inserir valor
+				# =================================================================
+					"Remover")
+						IDENTIFICACAO=$( dialog \
+							--stdout \
+							--title 'Remover contagem da data' \
+							--inputbox 'Insira a identificação da data a ser removida
+										Formato(DDMMMAAAA) Ex: 22set2020' \
+							0 0)
+
+						if [[ -z "$IDENTIFICACAO" ]]; then
+							dialog --title 'Erro!' --msgbox 'Nenhuma identificação inserida!' 6 45
+						else
+							ENTRADA=$(cat cofredecalorias.txt |
+								grep "$IDENTIFICACAO:" |
+								cut -d ":" -f1)
+
+							if [[ -z "$ENTRADA" ]]; then
+								dialog --title 'Erro!' --msgbox 'Identificação não existe!' 6 45
+							else
+								sed -i /"$ENTRADA:"/d cofredecalorias.txt
+								dialog --title 'Removido!' --msgbox 'Contagem removida com sucesso!' 6 45
+							fi
+						fi
+						;;
 			esac
-
 		fi
-
-
 done
 clear
